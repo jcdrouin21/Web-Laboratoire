4bcadbc5ebd3e2668d940798812c4ef183b10438
jcdrouin <jcdrouin21@icloud.com>
2020-02-11 18:39:55 -0500

    marche po

--- Labo4/app.js
+++ Labo4/app.js
@@ -1,10 +1,30 @@
-const request = new XMLHttpRequest();
-const url='https://glo3102lab4.herokuapp.com/';
+var userId;
 
 function createUser() {
-    request.open("POST", url + "Vincent", true);
-    request.send();
-    console.log(request.responseText);
-
+    var requestOptions = {
+        method: 'POST',
+        redirect: 'follow'
+    }
+    fetch('https://glo3102lab4.herokuapp.com/users ', requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            userId = JSON.parse(result).id;
+        })
+        .then(error => console.log('error', error));
 }
 
+function addTask(){
+    var task = document.getElementById("inputTask").value;
+
+
+    var requestOptions = {
+        method: 'POST',
+        redirect: 'follow',
+        method: 'cors',
+        header: new Headers({'name': task})
+    }
+    fetch('https://glo3102lab4.herokuapp.com/' + userId + '/tasks', requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .then(error => console.log('error', error));
+}
