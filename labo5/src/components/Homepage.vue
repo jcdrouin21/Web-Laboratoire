5c2b7e1e62079d83b00a95ab8a66cad0e7df154c
vincentlambert8 <vincent.lambert.8@ulaval.ca>
2020-02-27 11:51:25 -0500

    Ya un probleme

---
+++ labo5/src/components/Homepage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="greetingTitle">
+        <h2> The Ultimate Task Manager </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Homepage"
+    }
+</script>
+
+<style scoped>
+    #greetingTitle {
+        text-align: center;
+        margin: auto;
+        width:100%;
+    }
+    .center{
+        text-align: center;
+        margin: auto;
+        width:100%;
+    }
+    #inputTask{
+        width:100%;
+        height: 30%;
+        margin:auto;
+
+    }
+    .taskName{
+        text-align: center;
+        font-size: x-large;
+        border: 1px solid grey;
+        margin: 2%;
+        width: 90%;
+        display: block;
+    }
+
+    .taskName:hover{
+        border-color: dodgerblue;
+        cursor: pointer;
+    }
+</style>
