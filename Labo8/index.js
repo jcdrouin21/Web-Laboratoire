50dd7085f3c0ab5ce907fdd3c9e48caa479c7763
Yuxuan Zhao <yuxuan_none@live.com>
2020-04-02 23:13:00 -0400

    labo8 works

--- Labo8/index.js
+++ Labo8/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const uuid = require('uuid/v4');
+
 const port = process.env.PORT || 8080;
 const app = express();
 
 const users = [];
-
 
 app.set('views', __dirname + '/views');
 app.set('view_engine', 'ejs');
@@ -20,21 +20,26 @@ app.get('/login', (req, res) => res.render('login.ejs'));
 
 const validate = (token, username) => {
     return !!token &&
+        !!username &&
+        !!users[username] &&
         users[username].token === token;
 }
 app.get('/profile', (req, res) => {
     const token = req.cookies.labtoken;
-    if (!token || validate(token)) {
+    let username;
+    Object.keys(users).forEach( (name) => {
+        if (users[name].token === token) username = name
+    });
+    if (!validate(token, username)) {
         res.status(401).redirect('/login')
         return;
     }
-    res.render('userProfile.ejs', {username: user.username})
-})
+    res.render('userProfile.ejs', {username})
+});
 
 app.post('/users', (req, res) => {
-    const {username, password} = req.body;
-    const token = uuid();
-    users[req.body.username] = {password: req.body.password};
+    const { username, password } = req.body;
+    users[username] = {username: username, password: password};
     res.sendStatus(204);
 });
 
@@ -43,11 +48,11 @@ app.post('/login', (req, res) => {
     if (!!users[username] && users[username].password === password) {
         const token = uuid();
         users[username].token = token;
-        res.cookie('labtoken', token).send('Login successful')
+        res.cookie('labtoken', token).status(200).redirect('/profile');
         return;
     }
     res.status(401).send("Password or username invalid");
-})
+});
 
 
 app.listen(port, () => console.log(`Listening on port : ${port}`))
