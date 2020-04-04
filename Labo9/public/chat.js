c73e7af85c2b3eb11e14f9828796c7ca6804011b
vincentlambert8 <vincent.lambert.8@ulaval.ca>
2020-04-04 11:22:26 -0400

    Labo9 ui added

--- Labo9/public/chat.js
+++ Labo9/public/chat.js
@@ -1,15 +1,30 @@
 const socket = io.connect();
 
 let username;
+let users = [];
 document.getElementById('identificationButton').onclick = function() {
     username = document.getElementById('nameInput').value || 'user';
     document.getElementById('identificationBox').style.display = 'none';
     document.getElementById('messagesSection').style.display = 'block';
+    addUser(username);
 };
 
 function addMessage(user, messageContent) {
+    if(!(user in users)) {
+        users[user] = { user };
+        document.getElementById('usersBox').insertAdjacentHTML('beforeend',
+            `<div class="line"><span class="label label-primary">${user}</span></div>`);
+    }
     document.getElementById('chatBox').insertAdjacentHTML('beforeend',
         `<div class="line"><span class="label label-primary">${user}</span> ${messageContent}</div>`)
+}
+
+function addUser(user) {
+    if(!(user in users)) {
+        users[user] = { user };
+        document.getElementById('usersBox').insertAdjacentHTML('beforeend',
+            `<div class="line"><span class="label label-primary">${user}</span></div>`);
+    }
 }
 
 document.getElementById('sendMessageButton').onclick = function() {
@@ -20,7 +35,7 @@ document.getElementById('sendMessageButton').onclick = function() {
 };
 
 socket.on('getMessages', function (messages)  {
-    messages.forEach(m => addMessage(m.username, m.content))
+    messages.forEach(m => addMessage(m.username, m.content));
 });
 
 socket.on('getMessage', function (message) {
